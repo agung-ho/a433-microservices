c712225c024670a944eb585f3679c5f5a800b9eb
agung-ho <agungho@gmail.com>
2025-01-29 09:45:26 +0700

    add file Dockerfile and build_push_image_shipping_svc.sh

---
+++ build_push_image_shipping_svc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t agungho/shipping-svc:latest .
+docker image ls
+echo $PASSWORD_DOCKER_HUB | docker login -u agungho --password-stdin
+docker push agungho/shipping-svc:latest
